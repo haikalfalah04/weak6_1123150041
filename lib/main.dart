e2bfa2842d35ef0b33e6d80cebea779aa2741c53
Haikal Falah <haikalfalah04@gmail.com>
2025-11-02 21:25:53 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'screen/splash_screen_1.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "NIM : 1123150141, ilham",
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+      ),
+      home: const MySplashScreen(), // pastikan nama class cocok
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
---
+++ lib/screen/splash_screen_1.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class MySplashScreen extends StatelessWidget {
+  const MySplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 50),
+              Container(
+                width: 240,
+                height: 240,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.amber,
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/image.png"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+
+              // 🔹 Judul
+              const Text(
+                "Welcome",
+                style: TextStyle(
+                  fontSize: 32.0,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              const SizedBox(height: 10),
+
+              // 🔹 Subtitle 2 baris
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                child: Text(
+                  "Forgot to bring your wallet when you are shopping?\nDon’t worry, we’ve got you covered!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black54,
+                    height: 1.5,
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 40),
+
+              // 🔹 3 Bullet Indicator
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 15,
+                    height: 15,
+                    margin: const EdgeInsets.symmetric(horizontal: 4),
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Color(0xFF84a98c), // aktif
+                    ),
+                  ),
+                  Container(
+                    width: 15,
+                    height: 15,
+                    margin: const EdgeInsets.symmetric(horizontal: 4),
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Color(0xFFedede9), // non-aktif
+                    ),
+                  ),
+                  Container(
+                    width: 15,
+                    height: 15,
+                    margin: const EdgeInsets.symmetric(horizontal: 4),
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Color(0xFFedede9), // non-aktif
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
